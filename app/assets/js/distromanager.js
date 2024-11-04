a6d0810485d1b20fbbd44864432ea7feb9ee1915
Ninin <nininnory@gmail.com>
2024-11-04 13:02:36 +0100

    mise a jour
    changement url distromanager

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/NoryNinin/es/refs/heads/main/sample_distribution.json';
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/NoryNinin/distribution.json/refs/heads/main/sample_distribution.json';
 
 
 const api = new DistributionAPI(
